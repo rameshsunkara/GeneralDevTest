b97401b5849b50ac21dd2449821cf0ee96dd8ce8
Ramesh Sunkara <ramesh.sunkara@hpe.com>
2019-09-04 10:10:10 -0400

    Spring Boot Initializer

---
+++ src/main/java/com/rsunkara/gendevtest/GeneralDevTest/GeneralDevTestApplication.java
@@ -0,0 +1,13 @@
+package com.rsunkara.gendevtest.GeneralDevTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralDevTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralDevTestApplication.class, args);
+	}
+
+}
